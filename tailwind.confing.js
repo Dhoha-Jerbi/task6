6696f1a82119e53dad6f1b4bdd658e9b26e029de
Dhoha-Jerbi <143399437+Dhoha-Jerbi@users.noreply.github.com>
2023-11-10 15:45:25 +0100

    Add files via upload

---
+++ tailwind.confing.js
@@ -0,0 +1,22 @@
+module.exports={
+    content: 
+    ["./src/**/*.{html,js}"],
+    theme:{
+        screens: {
+            'sm': '600px',
+            // => @media (min-width: 576px) { ... }
+          
+            'md': '800px',
+            // => @media (min-width: 768px) { ... }
+          
+            'lg': '1024px',
+            // => @media (min-width: 992px) { ... }
+          
+    
+          },
+      
+        extend:{},
+    },
+    plugins:[],
+
+    }
